73ea26458fd4762c6a38fde042e328e682e28a03
Adrien P <placais@lpsc.in2p3.fr>
2024-04-12 21:30:32 +0200

    with autopairs

--- lua/configs/mini.lua
+++ lua/configs/mini.lua
@@ -1,3 +1,7 @@
+require('mini.pairs').setup({
+   -- TODO does not work as expected
+   ['"""'] = { action = 'closeopen', pair = '""""""', neigh_pattern = '[^\\].', register = { cr = false } },
+})
 require('mini.starter').setup({
    header = 'Bonjoure',
 })
