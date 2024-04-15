c102ff82c069f7c7ac30500fcc2b65a85ec2b18e
Adrien P <placais@lpsc.in2p3.fr>
2024-04-15 14:36:39 +0200

    removed mini basics as it modified too many keybindings

--- lua/configs/mini.lua
+++ lua/configs/mini.lua
@@ -1,3 +1,49 @@
+-- require('mini.basics').setup({
+--    {
+--       -- Options. Set to `false` to disable.
+--       options = {
+--          -- Basic options ('number', 'ignorecase', and many more)
+--          basic = false,
+--
+--          -- Extra UI features ('winblend', 'cmdheight=0', ...)
+--          extra_ui = false,
+--
+--          -- Presets for window borders ('single', 'double', ...)
+--          win_borders = 'default',
+--       },
+--
+--       -- Mappings. Set to `false` to disable.
+--       mappings = {
+--          -- TODO too many things I do not understand. Folding, go to def disappear
+--          -- Basic mappings (better 'jk', save with Ctrl+S, ...)
+--          basic = false,
+--
+--          -- Prefix for mappings that toggle common options ('wrap', 'spell', ...).
+--          -- Supply empty string to not create these mappings.
+--          option_toggle_prefix = [[\]],
+--
+--          -- TODO does not seem to work as it should
+--          -- Window navigation with <C-hjkl>, resize with <C-arrow>
+--          windows = false,
+--
+--          -- Move cursor in Insert, Command, and Terminal mode with <M-hjkl>
+--          move_with_alt = false,
+--       },
+--
+--       -- Autocommands. Set to `false` to disable
+--       -- TODO again, modifies too much for me
+--       autocommands = {
+--          -- Basic autocommands (highlight on yank, start Insert in terminal, ...)
+--          basic = false,
+--
+--          -- Set 'relativenumber' only in linewise and blockwise Visual mode
+--          relnum_in_visual_mode = false,
+--       },
+--
+--       -- Whether to disable showing non-error feedback
+--       silent = false,
+--    }
+-- })
 require('mini.pairs').setup()
 require('mini.starter').setup({
    header = 'Bonjoure',
