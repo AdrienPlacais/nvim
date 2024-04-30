25fc1c143c8392fe404a12b3d4393c49a4aba6cb
Adrien P <placais@lpsc.in2p3.fr>
2024-04-30 09:03:44 +0200

    avoid overriding of C-J and C-K

--- lua/configs/doge.lua
+++ lua/configs/doge.lua
@@ -13,9 +13,9 @@ let g:doge_python_settings = {
 -- Generate comment for current line
 map.set('n', '<Leader>dg', '<Plug>(doge-generate)')
 -- Interactive mode comment todo-jumping
-map.set('n', '<C-j>', '<Plug>(doge-comment-jump-forward)', opt)
+-- map.set('n', '<C-j>', '<Plug>(doge-comment-jump-forward)', opt)
 map.set('i', '<C-j>', '<Plug>(doge-comment-jump-forward)', opt)
 map.set('x', '<C-j>', '<Plug>(doge-comment-jump-forward)', opt)
-map.set('n', '<C-k>', '<Plug>(doge-comment-jump-backward)', opt)
+-- map.set('n', '<C-k>', '<Plug>(doge-comment-jump-backward)', opt)
 map.set('i', '<C-k>', '<Plug>(doge-comment-jump-backward)', opt)
 map.set('x', '<C-k>', '<Plug>(doge-comment-jump-backward)', opt)
