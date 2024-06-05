8bc0e2e80034b8db6e88cf7ff6f2ad67023c3a34
Adrien P <placais@lpsc.in2p3.fr>
2024-06-05 14:14:39 +0200

    added new elements, including all (?) DIAG to TW files

--- syntax/dat.lua
+++ syntax/dat.lua
@@ -9,42 +9,6 @@ endif
 
 syntax case ignore
 
-" " Constant parameters
-" syntax keyword twParam LATTICE FREQ SET_ADV END
-
-" " Basic elements
-" syntax keyword twSimpleElement DRIFT
-
-" syntax keyword twFocusElement SOLENOID QUAD
-
-" " Optimisation, diagnostics, errors
-" syntax keyword twOptimisation SPACE_CHARGE_COMP MATCH_FAM_GRAD
-" syntax keyword twOptimisation ADJUST ADJUST_STEERER STEERER
-" syntax keyword twOptimisation MATCH_FAM_LFOC
-
-" syntax keyword twDiagnostic DIAG_DENERGY DIAG_DPHASE DIAG_DSIZE
-" syntax keyword twDiagnostic DIAG_TWISS
-
-" syntax keyword twError ERROR_CAV_NCPL_STAT ERROR_BEAM_STAT
-
-" " Other
-" syntax keyword twExternalData FIELD_MAP
-
-" " Comments
-" syntax match twComment "\v;.*$"
-
-" " Colors
-" highlight link twParam Constant
-" highlight link twSimpleElement Identifier
-" highlight link twFocusElement Special
-" highlight link twOptimisation String
-" highlight link twDiagnostic Keyword
-" highlight link twError ErrorMsg
-" highlight link twExternalData PreProc
-" highlight link twComment Comment
-
-" Matching commands (structural)
-" Match one or more (\+) digits (\d)
 syntax match twElementLength '\d\+'          contained nextgroup=twElementAttr
 " Match one or more (\+) digits (\d) followed by a point (\.), and then again
 " a digit (\d) (or several, or none: \*)
@@ -67,11 +31,12 @@ syntax match twComment /;.*$/
 " Keyword commands
 syntax keyword twFieldMap FIELD_MAP                                                                                                             nextgroup=twFieldMapType skipwhite
 syntax keyword twElements DRIFT QUAD DTL_CEL                                                                                                    nextgroup=twElementLength skipwhite
-syntax keyword twCommands STEERER ADJUST_STEERER SET_BEAM_PHASE_ERROR ADJUST PLOT_DST SET_ADV FREQ FIELD_MAP_PATH SET_SYNC_PHASE                nextgroup=twCommandAttr
-syntax keyword twElementsNoLength LATTICE LATTICE_END FREQ MATCH_FAM_PHASE MATCH_FAM_GRAD MIN_PHASE_VARIATION DIAG_POSITION GAP THIN_STEERING APERTURE   nextgroup=twElementAttr
+syntax keyword twCommands SHIFT REPEAT_ELE SUPERPOSE_MAP STEERER ADJUST_STEERER ADJUST_STEERER_BX ADJUST_STEERER_BY SET_BEAM_PHASE_ERROR ADJUST PLOT_DST SET_ADV FREQ FIELD_MAP_PATH SET_SYNC_PHASE                nextgroup=twCommandAttr
+syntax keyword twElementsNoLength LATTICE LATTICE_END FREQ MATCH_FAM_PHASE MATCH_FAM_GRAD MIN_PHASE_VARIATION DIAG_POSITION GAP THIN_STEERING APERTURE CHOPPER  nextgroup=twElementAttr
 syntax keyword twOptimisation SPACE_CHARGE_COMP MATCH_FAM_GRAD ADJUST ADJUST_STEERER STEERER MATCH_FAM_LFOC                                     nextgroup=twDiagID skipwhite
-syntax keyword twDiagnostic DIAG_DENERGY DIAG_DPHASE DIAG_DSIZE DIAG_TWISS DIAG_WAIST DIAG_SIZE MARKER                                          nextgroup=twDiagWeight skipwhite
-syntax keyword twError ERROR_CAV_NCPL_STAT ERROR_BEAM_STAT ERROR_GAUSSIAN_CUT_OFF ERROR_QUAD_NCPL_STAT                                          nextgroup=twCommandAttr skipwhite
+syntax keyword twDiagnostic DIAG_CURRENT DIAG_DCURRENT DIAG_POSITION DIAG_DPOSITION DIAG_DIVERGENCE DIAG_DDIVERGENCE DIAG_SIZE_FWHM DIAG_SIZE DIAG_SIZEP DIAG_DSIZE__FWHM DIAG_DSIZE DIAG_DSIZE2_FWHM DIAG_DSIZE2 DIAG_DSIZE3 DIAG_DSIZE4 DIAG_DPSIZE2 DIAG_PHASE DIAG_ENERGY DIAG_DENERGY DIAG_DPHASE DIAG_LUMINOSITY DIAG_WAIST DIAG_ACHROMAT DIAG_EMIT DIAG_EMIT_99 DIAG_HALO DIAG_SET_MATRIX DIAG_TWISS DIAG_DTWISS DIAG_DTWISS2 DIAG_SEPARATION DIAG_SIZE_MAX DIAG_SIZE_MIN DIAG_PHASE_ADV DIAG_BETA DIAG_DBETA      nextgroup=twDiagWeight skipwhite
+syntax keyword twError ERROR_CAV_NCPL_STAT ERROR_BEAM_STAT ERROR_GAUSSIAN_CUT_OFF ERROR_QUAD_NCPL_STAT ERROR_QUAD_NCPL_DYN ERROR_CAV_NCPL_DYN   nextgroup=twCommandAttr skipwhite
+
 
 syntax region twAfterEnd start='^ *END' end='\%$'
 
