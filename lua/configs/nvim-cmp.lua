c2cde0a9483a0f34626ef3ed1bd0da3dcb03b5bf
Adrien P <placais@lpsc.in2p3.fr>
2024-06-05 10:57:05 +0200

    reconfigure placeholder navigation

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -19,31 +19,30 @@ cmp.setup({
       -- Scrollers
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),             -- show completion suggestions
-      ["<C-e>"] = cmp.mapping.abort(),                    -- close completion window
-      --
-      ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item (nothing if no item is selected)
-      --
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+
+      -- Accept currently selected item or go to next placeholder
+      ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+      -- Navigate snippets or confirm completion
       ['<C-j>'] = cmp.mapping(function(fallback)
-         if cmp.visible() then
-            cmp.confirm({ select = false }) -- <CR> behavior. To go to next node, needs to <C-e> first
-         elseif luasnip.locally_jumpable(1) then
-            luasnip.jump(1)                 -- Go to next node
+         if luasnip.jumpable(1) then
+            luasnip.jump(1)
+         elseif cmp.visible() then
+            cmp.confirm({ select = false })
          else
             fallback()
          end
       end, { "i", "s" }),
       ['<C-k>'] = cmp.mapping(function(fallback)
-         if cmp.visible() then
-            fallback()
-         elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1) -- Go to previous node
+         if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
          else
             fallback()
          end
       end, { "i", "s" }),
-   }),
-   -- sources for autocompletion
+   }), -- sources for autocompletion
    sources = cmp.config.sources({
       { name = "nvim_lsp" },
       { name = "luasnip" }, -- snippets
